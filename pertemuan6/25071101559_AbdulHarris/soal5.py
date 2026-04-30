037dd49ee1c4c74f2c728673901cb0a58ad59941
abdharris26 <abdharris246@gmail.com>
2026-04-30 10:26:41 +0700

    menambahkan folder

--- pertemuan6/25071101559_AbdulHarris/soal5.py
+++ pertemuan6/25071101559_AbdulHarris/soal5.py
@@ -1,5 +1,4 @@
-# Gabungan dari logika soal 1-4
-# (Simpan dalam satu file dan jalankan loop menu)
+
 inventaris = []
 katalog = [{'merk': 'Samsung', 'tipe': 'S23', 'sn': 'SAM01', 'stok': 2}]
 
@@ -9,16 +8,12 @@ while True:
     pilihan = input("Pilih menu: ")
     
     if pilihan == '1':
-        # Panggil fungsi registrasi (Soal 1)
         pass 
     elif pilihan == '2':
-        # Print tabel (Soal 5.2)
         pass
     elif pilihan == '3':
-        # Panggil update_stok (Soal 3)
         pass
     elif pilihan == '4':
-        # Panggil hitung_komisi (Soal 4)
         pass
     elif pilihan == '5':
         print("Terima kasih!")
